25ce2868df717859bfc4df660619ed52964b2963
gaurang003 <gaurangsharma.003@gmail.com>
2024-03-02 22:58:40 +0530

    add room type functionality changed

--- src/main/java/com/hotella/HotelBookingWebSite/controller/page/AdminRoomPageController.java
+++ src/main/java/com/hotella/HotelBookingWebSite/controller/page/AdminRoomPageController.java
@@ -1,10 +1,12 @@
 package com.hotella.HotelBookingWebSite.controller.page;
 
+import com.hotella.HotelBookingWebSite.entity.RoomType;
 import com.hotella.HotelBookingWebSite.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,12 +23,18 @@ public class AdminRoomPageController {
 
     @GetMapping
     public String getAdminRoomPage(Model model) {
+
+        model.addAttribute("isRoomAdded", false);
         model.addAttribute("roomTypes", roomService.getAllAvailableRoomTypes() );
         return PAGE;
     }
 
-    @PostMapping(ADD_ROOM_TYPE)
-    public String addRoomType(Model model) {
+    @PostMapping()
+    public String addRoomType(@ModelAttribute RoomType roomType, Model model) {
+
+        roomService.addRoomType(roomType);
+        model.addAttribute("isRoomAdded", true);
+        model.addAttribute("roomTypes", roomService.getAllAvailableRoomTypes() );
         return PAGE;
     }
 }
