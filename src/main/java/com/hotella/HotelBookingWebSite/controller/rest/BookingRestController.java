02a1c79c09dbab321cf8d57dbff218be89ff8914
Gaurang Sharma <gaurangsharma.003@gmail.com>
2026-05-11 23:16:51 +0530

    Few issues of Register and Refresh related issues

---
+++ src/main/java/com/hotella/HotelBookingWebSite/controller/rest/BookingRestController.java
@@ -0,0 +1,10 @@
+package com.hotella.HotelBookingWebSite.controller.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/room-booking")
+public class BookingRestController {
+
+}
---
+++ src/main/java/com/hotella/HotelBookingWebSite/controller/rest/FoodRestController.java
@@ -0,0 +1,41 @@
+package com.hotella.HotelBookingWebSite.controller.rest;
+
+import com.hotella.HotelBookingWebSite.dto.FoodDTO;
+import com.hotella.HotelBookingWebSite.entity.Food;
+import com.hotella.HotelBookingWebSite.entity.FoodCategory;
+import com.hotella.HotelBookingWebSite.service.FoodService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("rest/")
+public class FoodRestController {
+
+    @Autowired
+    FoodService foodService;
+
+    @GetMapping("food")
+    public List<Food> getAllFoods(){
+        return foodService.getAllFood();
+    }
+
+    @PostMapping("admin/food")
+    public Food saveFood(@Valid @RequestBody FoodDTO foodDTO) {
+        return foodService.saveFood(foodDTO);
+    }
+
+    @GetMapping("/category")
+    public List<FoodCategory> getAllFoodCategories(){
+        return foodService.getAllFoodCategory();
+    }
+
+    @PostMapping("admin/category")
+    public FoodCategory saveFoodCategory(@Valid  @RequestBody FoodCategory foodCategory) {
+        return foodService.saveFoodCategory(foodCategory);
+    }
+
+}
